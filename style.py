f42658153b4883ebdcf562eddfd394ab0060c50f
Erhei130 <59178102+Erhei130@users.noreply.github.com>
2021-04-20 16:29:15 -0400

    Create style.py

---
+++ style.py
@@ -0,0 +1,31 @@
+##################### style for the page ####################
+print('''<style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+form {text-align: center;}
+table {
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+
+th {
+  cursor: pointer;
+}
+
+th, td {
+  text-align: left;
+  padding: 16px;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2
+}
+</style></head>''')
+
+## and the green color
+print('''<div style="background-color:#8ebf42">''')
+
+##################### style (header) ####################
+print('''<h1 style="background-color:#93BFEA;">Food Compound-Human Protein Interaction</h1>''')
